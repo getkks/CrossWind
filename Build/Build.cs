942f4b7e38514b3a6ef966128ca9312af4d4a7dc
Karthik K Selvan <getkks@live.in>
2022-02-05 15:28:48 +0530

    Build Process - Simplified

--- Build/Build.cs
+++ Build/Build.cs
@@ -1,8 +1,4 @@
 using Nuke.Common.Execution;
-using Nuke.WebDocu;
-
-using static Nuke.Common.ChangeLog.ChangelogTasks;
-using static Nuke.Common.IO.PathConstruction;
 
 [CheckBuildProjectConfigurations]
 [ShutdownDotNetAfterServerBuild]
@@ -14,8 +10,8 @@ using static Nuke.Common.IO.PathConstruction;
 	OnPushBranchesIgnore = new[] { MainBranch, ReleaseBranchPrefix + "/*" },
 	OnPullRequestBranches = new[] { DevelopBranch, FeatureBranchPrefix, HotfixBranchPrefix },
 	PublishArtifacts = false,
-	InvokedTargets = new[] { nameof(ITest.Test), nameof(IPack.Pack) })]
-public partial class Build : NukeBuild, IReportCoverage, IHazArtifacts, IHazChangelog, IHazGitVersion, IHazSolution, IPack {
+	InvokedTargets = new[] { nameof(ITest.Test) })]
+public sealed partial class Build : NukeBuild, IReportCoverage, IHazArtifacts, IHazChangelog, IHazGitVersion, IHazSolution, IPublish {
 	private const string DevelopBranch = "develop";
 	private const string FeatureBranchPrefix = "feature";
 	private const string HotfixBranchPrefix = "hotfix";
@@ -25,7 +21,7 @@ public partial class Build : NukeBuild, IReportCoverage, IHazArtifacts, IHazChan
 	[CI] private readonly GitHubActions GitHubActions;
 	[Parameter] private readonly string GitHubToken;
 
-	[Solution(GenerateProjects = true)] private readonly Solution Solution;
+	//[Solution(GenerateProjects = true)] private readonly Solution Solution;
 	public bool CreateCoverageHtmlReport => true;
 
 	public Configure<ReportGeneratorSettings> ReportGeneratorSettings =>
@@ -35,45 +31,22 @@ public partial class Build : NukeBuild, IReportCoverage, IHazArtifacts, IHazChan
 		.SetHistoryDirectory(Path.Combine(From<IReportCoverage>().CoverageReportDirectory, "history"));
 
 	public bool ReportToCodecov => false;
-	Nuke.Common.ProjectModel.Solution IHazSolution.Solution => Solution;
+	//Nuke.Common.ProjectModel.Solution IHazSolution.Solution => Solution;
 
 	Target ITest.Test =>
 		 _ => _
 			.Inherit<ITest>()
 			.Partition(2);
 
-	IEnumerable<Project> ITest.TestProjects => Partition.GetCurrent(Solution.GetProjects("*.Tests"));
-
-	//TODO - Condition for checking Original Repository
-	private bool IsOriginalRepository => true;
-
-	private AbsolutePath OutputDirectory => RootDirectory / "Output";
-
-	private Target Pack1 =>
-		_ => _
-		.DependsOn(From<ICompile>().Compile).Produces(PackageDirectory / "*.nupkg")
-		.Executes(() =>
-			DotNetPack(_ => _
-				.SetProject(From<IHazSolution>().Solution)
-				.SetNoBuild(InvokedTargets.Contains(From<ICompile>().Compile))
-				.SetConfiguration(From<IHazConfiguration>().Configuration)
-				.SetOutputDirectory(PackageDirectory)
-				.SetVersion(From<IHazGitVersion>().Versioning.NuGetVersionV2)
-				.SetPackageReleaseNotes(GetNuGetReleaseNotes(ChangelogFile, From<IHazGitRepository>().GitRepository))));
-
-	private AbsolutePath PackageDirectory => OutputDirectory / "packages";
-	private AbsolutePath SourceDirectory => RootDirectory / "Source";
+	IEnumerable<Project> ITest.TestProjects => Partition.GetCurrent(From<IHazSolution>().Solution.GetProjects("*.Tests"));
 
 	public static int Main() => Execute<Build>(x => ( (ICompile) x ).Compile);
 
 	protected override void OnBuildInitialized() {
-		//Info($"\n\nBuilding version {From<IHazGitVersion>().Versioning.NuGetVersion} of {From<IHazSolution>().Solution.Name} ({From<IHazConfiguration>().Configuration}) using version {typeof(NukeBuild).Assembly.GetName().Version} of Nuke.");
-		Info(Terminal.IsRunningTerminal ? "LocalBuild" : "CI Build");
+		Info(Build.IsLocalBuild ? "LocalBuild" : "CI Build");
 		Info($"RunningOn {Environment.OSVersion.Platform switch { PlatformID.Unix => "Linux", PlatformID.MacOSX => "MacOS", _ => "Windows" }} - {Host}");
 		Info($"Branch: {From<IHazGitVersion>().Versioning.BranchName}\n\n");
 	}
-
-	/* GitRepository.Identifier == "nuke-build/nuke";*/
 
 	private static void Info( string info ) => Serilog.Log.Information(info);
 
--- Build/Global Usings.cs
+++ Build/Global Usings.cs
@@ -1,32 +1,19 @@
 global using System;
 global using System.Collections.Generic;
 global using System.IO;
-global using System.Linq;
 
 global using Nuke.Common;
 global using Nuke.Common.CI;
-global using Nuke.Common.CI.AzurePipelines;
 global using Nuke.Common.CI.GitHubActions;
-global using Nuke.Common.Git;
 global using Nuke.Common.IO;
 global using Nuke.Common.ProjectModel;
 global using Nuke.Common.Tooling;
 global using Nuke.Common.Tools.BenchmarkDotNet;
-global using Nuke.Common.Tools.Coverlet;
 global using Nuke.Common.Tools.DocFX;
-global using Nuke.Common.Tools.DotCover;
 global using Nuke.Common.Tools.DotNet;
 global using Nuke.Common.Tools.GitVersion;
-global using Nuke.Common.Tools.MSBuild;
 global using Nuke.Common.Tools.ReportGenerator;
 global using Nuke.Common.Tools.ReSharper;
-global using Nuke.Common.Utilities;
-global using Nuke.Common.Utilities.Collections;
 global using Nuke.Components;
 
-global using static Nuke.Common.IO.CompressionTasks;
-global using static Nuke.Common.IO.FileSystemTasks;
 global using static Nuke.Common.Tools.DocFX.DocFXBuildSettingsExtensions;
-global using static Nuke.Common.Tools.DotNet.DotNetTasks;
-global using static Nuke.Common.Tools.Git.GitTasks;
-global using static Nuke.Common.Tools.ReportGenerator.ReportGeneratorTasks;
